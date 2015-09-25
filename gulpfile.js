944a6d4d8a29be68d70a6aafcd7bafa6c2ace3b0
allanjnairn <allanjnairn@gmail.com>
2015-09-25 23:47:12 +0100

    css animation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
 
 gulp.task('sass', function() {
 	gulp.src('./scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./css'))
+		.pipe(livereload());
 });
 
+gulp.task('html', function() {
+	 return gulp.src([
+        './index.html'
+    ])
+    .pipe(livereload());
+});
+
+
 gulp.task('sass:watch', function(){
+	livereload.listen();
+	gulp.watch('./*.html', ['html']);
 	gulp.watch('./scss/*.scss', ['sass']);
 });
+
